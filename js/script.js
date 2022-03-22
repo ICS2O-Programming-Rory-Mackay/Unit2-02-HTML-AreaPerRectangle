8b876d929c4650e04d7124a9babe52fd0452dabc
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-03-22 15:05:21 +0000

    added maths

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Copyright (c) 2022 Rory Mackay All rights reserved
+// Created by: Mackay, Rory
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function calculateArea () {
+  // this function calculates area
+  document.getElementById('multiply-math').innerHTML = 'The area is ' + (8 * 3) + ' cm²'
+}
+
+function calculatePerimeter () {
+  // this function calculates perimeter
+  document.getElementById('add-math').innerHTML = 'The perimeter is ' + (8 * 2 + 3 * 2) + ' cm'
+}
